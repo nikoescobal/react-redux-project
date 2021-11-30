44a1b95701964b729632c61f6cfe9f916210c67b
Niko Escobal <niko.escobal@gmail.com>
2021-11-30 23:21:28 +0800

    Update Home component styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,69 @@
 import React from 'react';
 
 const Home = () => (
-  <div className="grid grid-cols-2 py-8">
-    <div className="col-span-2 h-1/ w-1/2 mx-auto">
+  <div className="grid grid-cols-2 lg:grid-cols-3 py-4">
+    <div className="col-span-2 lg:col-span-3 h-1/ w-1/2 mx-auto py-3">
       <img
         className="w-full h-full object-cover bg-cover"
         src="https://www.pngmart.com/files/13/My-Hero-Academia-Characters-PNG-Image-Background.png"
         alt=""
       />
     </div>
-    <h2 className="col-span-2 flex items-center justify-center bg-blue-500 p-4 text-white font-semi-bold text-3xl">
+    <h2 className="col-span-2 lg:col-span-3 flex items-center justify-center bg-blue-500 my-6 p-4 text-white font-semi-bold text-lg lg:text-3xl">
       Characters
     </h2>
-    <div className="flex px-6 py-3">
+    <div className="flex lg:px-6 justify-center px-2 py-3">
       <img
         src="https://storage.googleapis.com/my-hero-academia-api/Nemuri_Kayama.jpg"
         alt="avatar"
       />
       <div className="flex justify-center px-3 align-middle items-center h-full">
-        <h2 className="font-mono text-2xl font-semibold">Hero: Midnight</h2>
+        <h2 className="font-mono lg:text-2xl text-base font-semibold">Hero</h2>
       </div>
     </div>
-    <div className="flex px-6 py-3">
+    <div className="flex lg:px-6 justify-center px-2 py-3">
       <img
         src="https://storage.googleapis.com/my-hero-academia-api/Nemuri_Kayama.jpg"
         alt="avatar"
       />
       <div className="flex justify-center px-3 align-middle items-center h-full">
-        <h2 className="font-mono text-2xl font-semibold">Hero: Midnight</h2>
+        <h2 className="font-mono lg:text-2xl text-base font-semibold">Hero</h2>
       </div>
     </div>
-    <div className="flex px-6 py-3">
+    <div className="flex lg:px-6 justify-center px-2 py-3">
       <img
         src="https://storage.googleapis.com/my-hero-academia-api/Nemuri_Kayama.jpg"
         alt="avatar"
       />
       <div className="flex justify-center px-3 align-middle items-center h-full">
-        <h2 className="font-mono text-2xl font-semibold">Hero: Midnight</h2>
+        <h2 className="font-mono lg:text-2xl text-base font-semibold">Hero</h2>
       </div>
     </div>
-    <div className="flex px-6 py-3">
+    <div className="flex lg:px-6 justify-center px-2 py-3">
       <img
         src="https://storage.googleapis.com/my-hero-academia-api/Nemuri_Kayama.jpg"
         alt="avatar"
       />
       <div className="flex justify-center px-3 align-middle items-center h-full">
-        <h2 className="font-mono text-2xl font-semibold">Hero: Midnight</h2>
+        <h2 className="font-mono lg:text-2xl text-base font-semibold">Hero</h2>
+      </div>
+    </div>
+    <div className="flex lg:px-6 justify-center px-2 py-3">
+      <img
+        src="https://storage.googleapis.com/my-hero-academia-api/Nemuri_Kayama.jpg"
+        alt="avatar"
+      />
+      <div className="flex justify-center px-3 align-middle items-center h-full">
+        <h2 className="font-mono lg:text-2xl text-base font-semibold">Hero</h2>
+      </div>
+    </div>
+    <div className="flex lg:px-6 justify-center px-2 py-3">
+      <img
+        src="https://storage.googleapis.com/my-hero-academia-api/Nemuri_Kayama.jpg"
+        alt="avatar"
+      />
+      <div className="flex justify-center px-3 align-middle items-center h-full">
+        <h2 className="font-mono lg:text-2xl text-base font-semibold">Hero</h2>
       </div>
     </div>
   </div>
