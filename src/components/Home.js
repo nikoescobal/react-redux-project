c6fc2e10205237ae23dad6cfdf2959ece850b23a
Niko Escobal <niko.escobal@gmail.com>
2021-12-02 17:07:22 +0800

    Fix logic for filtering and displaying

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ import fetchData from '../store/actions';
 
 const Characters = () => {
   const characterStore = useSelector((state) => state.characters.characters);
-  const [filteredChars, setFilterChars] = useState(characterStore);
+  const [filteredChars, setFilterChars] = useState([]);
   const [selectedOption, setSelectedOption] = useState('All');
   const dispatch = useDispatch();
   let occupationsArr = characterStore.map((character) => character.occupation);
@@ -20,18 +20,22 @@ const Characters = () => {
     dispatch(fetchData());
   }, []);
 
-  // useEffect(() => {
-  //   setFilterChars(characterStore);
-  // });
+  useEffect(() => {
+    setFilterChars(characterStore);
+  }, [characterStore]);
 
   const update = (e) => {
     setSelectedOption(e.target.value);
-    const updated = filteredChars.filter(
-      (el) => el.occupation === e.target.value,
-    );
-    setFilterChars(updated);
+    console.log(e.target.value);
+    if (e.target.value === 'All') {
+      setFilterChars(characterStore);
+    } else {
+      const updated = characterStore.filter(
+        (el) => el.occupation === e.target.value,
+      );
+      setFilterChars(updated);
+    }
   };
-  console.log(filteredChars);
   return (
     <div className="grid grid-cols-2 grid-gap-2 lg:grid-cols-4 py-4 bg-yellow-50">
       <div className="col-span-2 lg:col-span-4 h-1/ w-1/2 border rounded border-red-500 mx-auto">
@@ -41,7 +45,12 @@ const Characters = () => {
           alt="hero-bg"
         />
       </div>
-      <select value={selectedOption} onChange={update}>
+      <select
+        className="m-8 p-3 bg-blue-500 text-white"
+        value={selectedOption}
+        onChange={update}
+      >
+        <option value="All">All</option>
         {occupationsArr.map((item, index) => (
           <option key={index} value={item}>
             {item}
@@ -55,23 +64,17 @@ const Characters = () => {
         && filteredChars.map((character) => {
           const { id, name, images } = character;
           return (
-            <>
-              <Link key={id} to={{ pathname: `/id/${id}` }}>
-                <div key={id} className="">
-                  <h2 className="font-mono lg:text-2xl text-white bg-yellow-400 mx-0.5 flex justify-center items-center text-base font-semibold">
-                    {' '}
-                    {name}
-                  </h2>
-                </div>
-                <div className="flex lg:px-6 bg-blue-500 text-white shadow justify-center px-2 py-3">
-                  <img
-                    className="h-96 object-cover"
-                    src={images[0]}
-                    alt={name}
-                  />
-                </div>
-              </Link>
-            </>
+            <Link key={id} to={{ pathname: `/id/${id}` }}>
+              <div key={id} className="">
+                <h2 className="font-mono lg:text-2xl text-white bg-yellow-400 mx-0.5 flex justify-center items-center text-base font-semibold">
+                  {' '}
+                  {name}
+                </h2>
+              </div>
+              <div className="flex lg:px-6 bg-blue-500 text-white shadow justify-center px-2 py-3">
+                <img className="h-96 object-cover" src={images[0]} alt={name} />
+              </div>
+            </Link>
           );
         })}
     </div>
