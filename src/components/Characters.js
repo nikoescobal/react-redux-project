a86304c33753a5b49847bafd3030d45c70d20bca
Niko Escobal <niko.escobal@gmail.com>
2021-12-02 21:59:16 +0800

    Update font for better responsiveness in characters section

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -27,7 +27,7 @@ const Characters = () => {
           />
         </div>
         <div className="flex w-1/2 md:w-3/5 lg:w-4/5 flex-col px-2 lg:px-3 xl:px-4 py-2 ">
-          <div className="lg:text-2xl md:text-lg text-base text-white mb-6 align-middle rounded flex w-full">
+          <div className="lg:text-2xl text-sm md:text-base text-white mb-6 align-middle rounded flex w-full">
             <h2 className="font-semibold p-2 lg:p-3 xl:p-4 bg-blue-700">
               Name:
               {' '}
@@ -38,7 +38,7 @@ const Characters = () => {
               {characters.name}
             </h2>
           </div>
-          <div className="lg:text-lg text-base text-white p-4 mb-6 rounded bg-blue-400">
+          <div className="lg:text-lg  text-sm md:text-base text-white p-4 mb-6 rounded bg-blue-400">
             <h2 className="font-semibold text-blue-700">Alias:</h2>
             <h2 className="font-bold p-3 bg-blue-400">
               {' '}
@@ -47,7 +47,7 @@ const Characters = () => {
               {characters.alias}
             </h2>
           </div>
-          <div className="lg:text-lg text-base text-white p-4 mb-6 rounded bg-blue-400">
+          <div className="lg:text-lg  text-sm md:text-base text-white p-4 mb-6 rounded bg-blue-400">
             <h2 className="font-semibold text-blue-700">Gender:</h2>
             <h2 className="font-bold p-3 bg-blue-400">
               {' '}
@@ -56,7 +56,7 @@ const Characters = () => {
               {characters.gender}
             </h2>
           </div>
-          <div className="lg:text-lg text-base text-white p-4 mb-6 rounded bg-blue-400">
+          <div className="lg:text-lg  text-sm md:text-base text-white p-4 mb-6 rounded bg-blue-400">
             <h2 className="font-semibold text-blue-700">Description:</h2>
             <h2 className="font-bold p-3 bg-blue-400">
               {' '}
@@ -65,7 +65,7 @@ const Characters = () => {
               {characters.description}
             </h2>
           </div>
-          <div className="lg:text-lg text-base text-white p-4 mb-6 rounded bg-blue-400">
+          <div className="lg:text-lg  text-sm md:text-base text-white p-4 mb-6 rounded bg-blue-400">
             <h2 className="font-semibold text-blue-700">Occupation:</h2>
             <h2 className="font-bold p-3 bg-blue-400">
               {' '}
@@ -73,7 +73,7 @@ const Characters = () => {
               {characters.occupation}
             </h2>
           </div>
-          <div className="lg:text-lg text-base text-white p-4 mb-6 rounded bg-blue-400">
+          <div className="lg:text-lg  text-sm md:text-base text-white p-4 mb-6 rounded bg-blue-400">
             <h2 className="font-semibold text-blue-700">Quirk:</h2>
             <h2 className="font-bold p-3 bg-blue-400">
               {' '}
@@ -81,7 +81,7 @@ const Characters = () => {
               {characters.quirk}
             </h2>
           </div>
-          <div className="lg:text-lg text-base text-white p-4 mb-6 rounded  bg-blue-400">
+          <div className="lg:text-lg text-sm md:text-base text-white p-4 mb-6 rounded  bg-blue-400">
             <h2 className="font-semibold text-blue-700">Status:</h2>
             <h2 className="font-bold p-3 bg-blue-400">
               {' '}
